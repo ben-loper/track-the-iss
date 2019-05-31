5ecfb1a34a0076e2b2c62129ffb0bbe0d1569173
Ben Loper <loperbp@gmail.com>
2019-05-31 16:02:06 -0400

    Add fetch to get the ISS position and display the long and lat on the page. Add refresh to get the current location every 3 seconds

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('https://api.wheretheiss.at/v1/satellites/25544')
+    .then(resp => {
+      return resp.json();
+    })
+    .then(location => {
+      updateSite(location);
+    });
+});
+
+setInterval(() => {
+  fetch('https://api.wheretheiss.at/v1/satellites/25544')
+    .then(resp => {
+      return resp.json();
+    })
+    .then(location => {
+      updateSite(location);
+    });
+}, 3000);
+
+function updateSite(location) {
+  const latTag = document.getElementById('latCord');
+  const longTag = document.getElementById('longCord');
+
+  latTag.innerText = `Latitude: ${location.latitude}`;
+  longTag.innerText = `Longitude: ${location.longitude}`;
+}
